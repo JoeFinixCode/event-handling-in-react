d7a48c9517d14936bb2737d48025c1433dc7c5ab
Joe Finix <joefinixcode@gmail.com>
2020-10-07 23:25:18 +0800

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,35 @@
-import React from "react";
+// Event Handling practice
+import React, { useState } from 'react';
 
 function App() {
+  const [headingText, setHeadingText] = useState('Hello');
+  const [isMousedOver, setMouseOver] = useState(false);
+
+  function handleClick() {
+    setHeadingText('Submitted');
+  }
+
+  function hover() {
+    setMouseOver(true);
+  }
+
+  function exitHover() {
+    setMouseOver(false);
+  }
+
   return (
-    <div className="container">
-      <h1>Hello</h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+    <div className='container'>
+      <h1>{headingText}</h1>
+      <input type='text' placeholder="What's your name?" />
+      <button
+        style={{ backgroundColor: isMousedOver ? 'black' : 'white' }}
+        onClick={handleClick}
+        onMouseOver={hover}
+        // this function is being called if mouse exits hover
+        onMouseOut={exitHover}
+      >
+        Submit
+      </button>
     </div>
   );
 }
